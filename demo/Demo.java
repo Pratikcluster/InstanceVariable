649683a2c5984c82732429045e6dd2151b24e5d2
Praticluster <pratikcluster@gmail.com>
2022-01-16 10:13:44 +0530

    Instance variable

---
+++ demo/Demo.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+public class Demo {
+    int i = 10;
+    int j = 20;
+    int k = 30;
+
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        System.out.println(d.i);
+        System.out.println(d.j);
+        System.out.println(d.k);
+
+        Demo Pratik = new Demo();
+        System.out.println(Pratik.i);
+        System.out.println(Pratik.j);
+        System.out.println(Pratik.k);
+
+        Demo a = new Demo();
+        System.out.println(a.i);
+        System.out.println(a.j);
+        System.out.println(a.k);
+    }
+}
---
+++ friday/Friday.java
@@ -0,0 +1,7 @@
+package com.friday;
+
+public class Friday {
+    public static void main(String[] args) {
+        System.out.println("Hello Friday");
+    }
+}
